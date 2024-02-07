c480ce580733a7b5dacde547619470cf82d0e366
Mydoom <109018242+AnshumanSingh116@users.noreply.github.com>
2024-02-07 17:02:24 +0530

    script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function generateMessage(noCount) {
 }
 
 function changeImage(image) {
-  catImg.src = `img/cat-${image}.jpg`;
+  catImg.src = `cat-${image}.jpg`;
 }
 
 function updateNoButtonText() {
